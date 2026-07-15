cd0bd06e2f9c15f76863a4ffe97ca50ccb9ddefe
Vipul Kumar <47377393+vipul513@users.noreply.github.com>
2026-07-16 03:11:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# This code is from https://github.com/LondheShubham153/flask-app-ecs/tree/main
+# Flask App
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
